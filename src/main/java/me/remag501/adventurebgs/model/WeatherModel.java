fafc1981b4164eebbad094b72567ecbb1697d484
Remag501-Desktop <evanverma2003@gmail.com>
2025-11-26 15:05:47 -0800

    feat(weather): Model stores data for weather conditions.

--- src/main/java/me/remag501/adventurebgs/model/WeatherModel.java
+++ src/main/java/me/remag501/adventurebgs/model/WeatherModel.java
@@ -1,4 +1,53 @@
 package me.remag501.adventurebgs.model;
 
 public class WeatherModel {
+
+    private final String type;
+    private final String world;
+
+    private final int minDurationSeconds;
+    private final int maxDurationSeconds;
+
+    private final int minFrequencySeconds;
+    private final int maxFrequencySeconds;
+
+    public WeatherModel(
+            String type,
+            String world,
+            int minDurationSeconds,
+            int maxDurationSeconds,
+            int minFrequencySeconds,
+            int maxFrequencySeconds
+    ) {
+        this.type = type;
+        this.world = world;
+        this.minDurationSeconds = minDurationSeconds;
+        this.maxDurationSeconds = maxDurationSeconds;
+        this.minFrequencySeconds = minFrequencySeconds;
+        this.maxFrequencySeconds = maxFrequencySeconds;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public int getMinDurationSeconds() {
+        return minDurationSeconds;
+    }
+
+    public int getMaxDurationSeconds() {
+        return maxDurationSeconds;
+    }
+
+    public int getMinFrequencySeconds() {
+        return minFrequencySeconds;
+    }
+
+    public int getMaxFrequencySeconds() {
+        return maxFrequencySeconds;
+    }
 }
