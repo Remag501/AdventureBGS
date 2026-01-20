d708afe39d0d8f8f82f05de2016ac10f48fe986f
Remag501-Desktop <evanverma2003@gmail.com>
2026-01-19 18:48:32 -0800

    test: added boiler code for unit testing, and setup build in this editor.

---
+++ src/test/java/me/remag501/adventurebgs/listeners/ExtractionListenerTest.java
@@ -0,0 +1,14 @@
+package me.remag501.adventurebgs.listeners;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExtractionListenerTest {
+
+    @Test
+    void playerExtractTest() {
+        assertEquals(1, 1);
+    }
+
+}
